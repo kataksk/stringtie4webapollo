69f0eb254375dc52e085ece97dbff91ffcd98f3d
Kosuke Kataoka <38752733+kataksk@users.noreply.github.com>
2022-02-06 14:57:00 +0900

    Add files via upload

---
+++ stringtie4webapollo.py
@@ -0,0 +1,25 @@
+import sys
+import re
+
+stringtie_gtf_input = sys.argv[1]
+
+def converter(feature, type):
+    if type == 'transcript':
+        feature_split = feature.split('"; ')
+        id_start = feature_split[1].find('"')
+        output = 'ID=' + feature_split[1][id_start+1:]
+        return output
+    elif type == 'exon':
+        feature_split = feature.split('"; ')
+        id_start = feature_split[1].find('"')
+        output = 'ID=' + feature_split[1][id_start+1:] + '.exon;Parent=' + feature_split[1][id_start+1:]
+        return output
+
+with open(stringtie_gtf_input, 'r') as f:
+    for line in f:
+        if line[0] == '#':
+            continue
+        else:
+            line = line.rstrip('\n').split('\t')
+            output = '\t'.join(line[0:8]) + '\t' + converter(line[8], line[2])
+            print(output)
